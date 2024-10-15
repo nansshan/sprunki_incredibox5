237b2be813f752216a2d0f14eff6b21be68a7a61
zhumengguang <zhumengguang@sparkedu.com>
2024-10-15 14:22:35 +0800

    修改

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-W8Y72G17E5"/>
+      <Script id='google-analytics' strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W8Y72G17E5');
+        `}
+      </Script>
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -54,6 +55,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <GoogleAnalytics />
       </body>
     </html>
   );
